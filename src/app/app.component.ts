ca8cdb236bc0256c3383d4e2c00814982e679c40
Shadumbra <alex.bulc.96@gmail.com>
2024-03-07 18:45:41 +0100

    header sidebar main y footer acabados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ContentComponent } from './components/content/content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, SidebarComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    ContentComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
