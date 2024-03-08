7394daf8976a7777940944d521d0261387ebd4f3
Shadumbra <alex.bulc.96@gmail.com>
2024-03-08 17:54:08 +0100

    home y clima terminados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ClimaComponent } from './pages/clima/clima.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'clima', component: ClimaComponent },
+
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
