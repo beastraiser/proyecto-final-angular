f65871fc531ffabfc9e471d8435d81a7caf4e251
Shadumbra <alex.bulc.96@gmail.com>
2024-03-09 10:51:12 +0100

    ahoracado component terminado a falta de estilar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { ClimaComponent } from './pages/clima/clima.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AdivinanzaComponent } from './pages/adivinanza/adivinanza.component';
+import { AhorcadoComponent } from './pages/ahorcado/ahorcado.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'clima', component: ClimaComponent },
   { path: 'adivinanza', component: AdivinanzaComponent },
+  { path: 'ahorcado', component: AhorcadoComponent },
 
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
