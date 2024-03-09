1cf36a7351377d5ffe2607bbfa5f4922ff5c4103
Shadumbra <alex.bulc.96@gmail.com>
2024-03-09 10:25:57 +0100

    anotaciones apis

--- src/app/pages/clima/clima.component.ts
+++ src/app/pages/clima/clima.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ClimaService } from '../../services/clima.service';
 
 @Component({
   selector: 'app-clima',
@@ -16,12 +17,18 @@ export class ClimaComponent {
   datosClima: any = {};
   ciudad: string = ''; // Definición de la propiedad ciudad
 
-  constructor() {}
+  constructor(private climaService: ClimaService) {}
 
   buscarClima(ciudad: string) {
-    if (ciudad) {
+    /* if (ciudad) {
       this.fetchDatosClima(ciudad);
-    }
+    } */
+    // nos suscribimos a la llamada, recibimos los datos y los mostramos
+    this.climaService.buscarClima(ciudad).subscribe((data) => {
+      //🐱‍👤: console.log es una ayuda que nos permite ver los que le metamos en la CONSOLA del navegador (f12 en navegador), es muy util :)
+      console.log('ver datos:', data);
+      this.mostrarDatosClima(data);
+    });
   }
 
   fetchDatosClima(ciudad: string) {
