b6c1a4af74f425155d50583d5ab59e1e31bd84a9
Shadumbra <alex.bulc.96@gmail.com>
2024-03-09 13:19:15 +0100

    4 apis funcionales terminadas a falta de estilar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,15 @@ import { AdivinanzaComponent } from './pages/adivinanza/adivinanza.component';
 import { AhorcadoComponent } from './pages/ahorcado/ahorcado.component';
 import { CalculadorabmiComponent } from './pages/calculadorabmi/calculadorabmi.component';
 import { PokeapiComponent } from './pages/pokeapi/pokeapi.component';
+import { CocktailComponent } from './pages/cocktail/cocktail.component';
+import { DogsComponent } from './pages/dogs/dogs.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'clima', component: ClimaComponent },
   { path: 'pokeapi', component: PokeapiComponent },
+  { path: 'cocktail', component: CocktailComponent },
+  { path: 'dogs', component: DogsComponent },
   { path: 'adivinanza', component: AdivinanzaComponent },
   { path: 'ahorcado', component: AhorcadoComponent },
   { path: 'calculadorabmi', component: CalculadorabmiComponent },
