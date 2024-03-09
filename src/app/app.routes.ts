5237a6a4925d2bae38d2e6e2506143b381e22c19
Shadumbra <alex.bulc.96@gmail.com>
2024-03-09 11:03:48 +0100

    calculadora terminada a falta de estilar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { ClimaComponent } from './pages/clima/clima.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AdivinanzaComponent } from './pages/adivinanza/adivinanza.component';
 import { AhorcadoComponent } from './pages/ahorcado/ahorcado.component';
+import { CalculadorabmiComponent } from './pages/calculadorabmi/calculadorabmi.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'clima', component: ClimaComponent },
   { path: 'adivinanza', component: AdivinanzaComponent },
   { path: 'ahorcado', component: AhorcadoComponent },
+  { path: 'calculadorabmi', component: CalculadorabmiComponent },
 
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
