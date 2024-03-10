b41df1737392a75b84492eb70fe8eca7faabddee
Shadumbra <alex.bulc.96@gmail.com>
2024-03-10 14:18:20 +0100

    buscador implementado y navbar cambiado

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { SearchService } from '../../services/search.service';
 import { FormsModule } from '@angular/forms';
 
 @Component({
--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -5,12 +5,25 @@ import { Subject } from 'rxjs';
   providedIn: 'root',
 })
 export class SearchService {
-  private searchQuerySubject = new Subject<string>();
-  searchQuery$ = this.searchQuerySubject.asObservable();
+  private elementos: { nombre: string; icono: string }[] = [
+    { nombre: 'clima', icono: 'thermometer-snow' },
+    { nombre: 'pokeapi', icono: 'film' },
+    { nombre: 'cocktail', icono: 'heart' },
+    { nombre: 'dogs', icono: 'bricks' },
+    // Agrega más elementos aquí con sus respectivos iconos
+  ];
 
   constructor() {}
 
-  search(query: string) {
-    this.searchQuerySubject.next(query);
+  buscarElemento(query: string): { nombre: string; icono: string }[] {
+    // Aquí implementa la lógica de búsqueda según tu aplicación
+    // Por ejemplo, puedes filtrar los elementos basados en la consulta
+    return this.elementos.filter((elemento) =>
+      elemento.nombre.toLowerCase().includes(query.toLowerCase())
+    );
+  }
+
+  obtenerTodosElementos(): { nombre: string; icono: string }[] {
+    return this.elementos;
   }
 }
